8a87d003997e2d9f78be619d2e60f7db68fcd2ef
Andrew <andrew.mk4@yahoo.com>
2021-04-08 01:02:25 -0700

    update

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
       backgroundImage: 'url("https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Fcountvoncount&psig=AOvVaw0UPIL398wSNqwl8tJ9WMfS&ust=1617947362069000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjQ5_T57e8CFQAAAAAdAAAAABAL")'
     }}>
       <Nav className="navRoute" />
-      <Footer className="footContainer" />
       <h1>Counts' Counter</h1>
         <div>
           <ExtraLayer />
         </div>
       <h2>Hello</h2>
+      <Footer className="footer" />
     </div>
   );
 }
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = (props) => {
     return (
         <div
-            className="footer">
+            className="foot">
             <a href="#top">Top of Page</a>
         </div>
     )
