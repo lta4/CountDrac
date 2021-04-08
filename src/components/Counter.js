14182defea3949aecd88031229f930223a855714
Andrew <andrew.mk4@yahoo.com>
2021-04-08 15:21:52 -0700

    hover buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,13 +7,13 @@ const Counter = (props) => {
 
     return <div>
         <h1>{contextData.state.counter}</h1>
-        <button onClick={() => contextData.dispatch({type: "add", payload: 1})}>Add One</button>
-        <button onClick={() => contextData.dispatch({type: "add", payload: 2})}>Add Two</button>
-        <button onClick={() => contextData.dispatch({type: "subtract", payload: 1})}>Subtract One</button>
-        <button onClick={() => contextData.dispatch({type: "subtract", payload: 2})}>Subtract Two</button>
-        <button onClick={() => contextData.dispatch({type: "multiply", payload: 2})}>Multiply Two</button>
-        <button onClick={() => contextData.dispatch({type: "multiply", payload: 0})}>Multiply Zero</button>
-        <button onClick={() => contextData.dispatch({type: "divide", payload: 2})}>Divide Two</button>
+        <button onClick={() => contextData.dispatch({type: "add", payload: 1})} className="button">Add One</button>
+        <button onClick={() => contextData.dispatch({type: "add", payload: 2})} className="button">Add Two</button>
+        <button onClick={() => contextData.dispatch({type: "subtract", payload: 1})} className="button">Subtract One</button>
+        <button onClick={() => contextData.dispatch({type: "subtract", payload: 2})} className="button">Subtract Two</button>
+        <button onClick={() => contextData.dispatch({type: "multiply", payload: 2})} className="button">Multiply Two</button>
+        <button onClick={() => contextData.dispatch({type: "multiply", payload: 0})} className="button">Multiply Zero</button>
+        <button onClick={() => contextData.dispatch({type: "divide", payload: 2})} className="button">Divide Two</button>
     </div>
 }
 
