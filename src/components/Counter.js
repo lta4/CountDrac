66b95ba98e827c0df10149f444a32c61d5e5efbb
Andrew <andrew.mk4@yahoo.com>
2021-04-11 01:51:57 -0700

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,7 +7,7 @@ const Counter = (props) => {
 
     return <div>
         <h1>{contextData.state.counter}</h1>  
-        <button onClick={() => contextData.dispatch({type: "add", payload: 1})} className="buttonOne">Add 1</button>
+        <button onClick={() => contextData.dispatch({type: "add", payload: 1})} className="buttonOne" id="one">Add 1</button>
         <button onClick={() => contextData.dispatch({type: "add", payload: 2})} className="button">Add 2</button>
         <button onClick={() => contextData.dispatch({type: "subtract", payload: 1})} className="button">Subtract 1</button>
         <button onClick={() => contextData.dispatch({type: "subtract", payload: 2})} className="button">Subtract 2</button>
