8b4113738ece198990e14d935824165a62046a23
Andrew <andrew.mk4@yahoo.com>
2021-05-14 03:08:28 -0700

    update

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
         <div>
           <ExtraLayer />
         </div>
-      <h2>Hello</h2>
-      <h3>to the</h3>
-      <h3>captain america</h3>
+      <h2>We do the counting for you!</h2>
+      <h3>Follow the storyline</h3>
+      <h3>Postgres backend with storyline!</h3>
       </Route>
       </Switch>
       <Footer />
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,15 +7,15 @@ const Counter = (props) => {
 
     return <div className="buttons">
         <h1>{contextData.state.counter}</h1>  
-        <button onClick={() => contextData.dispatch({type: "add", payload: 1})} className="button" id="one">Add 1</button>
-        <button onClick={() => contextData.dispatch({type: "add", payload: 2})} className="button">Add 2</button>
-        <button onClick={() => contextData.dispatch({type: "subtract", payload: 1})} className="button">Subtract 1</button>
-        <button onClick={() => contextData.dispatch({type: "subtract", payload: 2})} className="button">Subtract 2</button>
-        <button onClick={() => contextData.dispatch({type: "multiply", payload: 2})} className="button">Multiply 1</button>
-        <button onClick={() => contextData.dispatch({type: "multiply", payload: 2})} className="button">Multiply 2</button>
-        <button onClick={() => contextData.dispatch({type: "multiply", payload: 0})} className="button">Multiply 0</button>
-        <button onClick={() => contextData.dispatch({type: "divide", payload: 2})} className="button">Divide 1</button>
-        <button onClick={() => contextData.dispatch({type: "divide", payload: 2})} className="button">Divide 2</button>
+        <button onClick={() => contextData.dispatch({type: "add", payload: 1})} className="button" id="one">+ 1</button>
+        <button onClick={() => contextData.dispatch({type: "add", payload: 2})} className="button">+ 2</button>
+        <button onClick={() => contextData.dispatch({type: "subtract", payload: 1})} className="button">- 1</button>
+        <button onClick={() => contextData.dispatch({type: "subtract", payload: 2})} className="button">- 2</button>
+        <button onClick={() => contextData.dispatch({type: "multiply", payload: 2})} className="button">* 1</button>
+        <button onClick={() => contextData.dispatch({type: "multiply", payload: 2})} className="button">* 2</button>
+        <button onClick={() => contextData.dispatch({type: "multiply", payload: 0})} className="button">* 0</button>
+        <button onClick={() => contextData.dispatch({type: "divide", payload: 2})} className="button">% 1</button>
+        <button onClick={() => contextData.dispatch({type: "divide", payload: 2})} className="button">% 2</button>
     </div>
 }
 
