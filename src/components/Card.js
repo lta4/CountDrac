72b5e3850b967d0e2a0edaba88957ae5bf74b7d5
Andrew <andrew.mk4@yahoo.com>
2021-06-09 02:21:55 -0700

    updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,13 +5,13 @@ const Card = () => {
 
     const [ name, setName ] = useState("Your Name");
     const [ job, setJob ] = useState("Job Title");
-    const [ about, setAbout ] = useState("Blah Blah Blah");
+    const [ about, setAbout ] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
     return (
         <div className="Card">
             <div className="upper-container">
                 <div className="image-container">
-                    <img src=" " alt='' height="100px" width="100px" />
+                    <img src="https://res.cloudinary.com/dfltkqotn/image/upload/v1614972036/IMG_2031_ygt8yt.png" alt='' height="100px" width="100px" />
                 </div>
             </div>
             <div className="lower-container">
