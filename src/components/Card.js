66486cb81d351eae23c75337460044dbe374ef7b
Andrew <andrew.mk4@yahoo.com>
2021-06-09 00:14:10 -0700

    card bstructure

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import "./Card.css"
+
+const Card = () => {
+
+    const [ name, setName ] = useState("Your Name");
+    const [ job, setJob ] = useState("Job Title");
+    const [ about, setAbout ] = useState("Blah Blah Blah");
+
+    return (
+        <div className="Card">
+            <div className="uppder-container">
+                <div className="image-container">
+                    <img src=" " alt='' height="100px" width="100px" />
+                </div>
+            </div>
+            <div className="lower-container">
+                <h3> { name } </h3>
+                <h4> { job } </h4>
+                <p> { about } </p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Card from "../components/Card"
 
 const About = () => {
     
     return (
         <div>
             <h1>Hello About</h1>
+            <Card />
         </div>
     );
 };
