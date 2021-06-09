19bd51aae36002c8fda8a183977c57f31467ed01
Andrew <andrew.mk4@yahoo.com>
2021-06-09 03:00:09 -0700

    update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -11,9 +11,9 @@ const Main = (props) => {
                 <div>
                 <ExtraLayer />
                 </div>
-                <h2>We do the counting for you!</h2>
-                    <h3>Follow the storyline</h3>
-                    <h3>Postgres backend with storyline!</h3>
+            <h2>We do the counting for you!</h2>
+                <h3>Follow the storyline</h3>
+                <h3>Postgres backend with storyline!</h3>
         </Route>
         </div>
     )
