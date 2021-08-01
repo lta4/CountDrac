642bccddb3e33b65bcccb642895698dd7e5d5a6c
Andrew <andrew.mk4@yahoo.com>
2021-08-01 03:18:05 -0700

    minor updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,13 @@ import Card from "../components/Card"
 
 const About = () => {
 
-    const [ name, setName ] = useState("Your Name");
-    const [ job, setJob ] = useState("Job Title");
-    const [ about, setAbout ] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    const [ name, setName ] = useState("Andrew Arias");
+    const [ job, setJob ] = useState("Full Stack Developer");
+    const [ about, setAbout ] = useState("I'm Andrew Arias. A Full Stack Developer and devoted husband and father. While taking on challenges which require a good sense of perseverance, I am able to convey my creative abilities while accomplishing any tasks. Let’s make our goals a reality.");
     
     return (
         <div>
-            <Card name={ 'Andrew Arias' } job={ 'Full Stack Developer' } about= { about }/>
+            <Card name= { name } job={ job } about= { about }/>
         </div>
     );
 };
